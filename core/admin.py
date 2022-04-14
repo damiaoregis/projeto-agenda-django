a8d348ab6d9de8c7111c9843be38c3eb0674075b
damiaoregis <damiao.v.serrana@gmail.com>
2022-04-14 20:04:19 -0300

    Incluido funções de alterar e excluir

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_display = ('titulo', 'local_evento', 'data_evento', 'data_criacao')
     list_filter = ('usuario', 'data_evento')
 
 admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
+    local_evento = models.CharField(blank=True, max_length=100)
     descricao = models.TextField(blank=True)
     data_evento = models.DateTimeField(verbose_name='Data do evento')
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de criaÇão')
@@ -18,3 +19,6 @@ class Evento(models.Model):
  
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%MHrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
